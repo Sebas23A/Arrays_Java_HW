05d85ef3b60241a5376440363ef2112bd8ca1eb7
sebastianaguas <sebas23_aguas@hotmail.com>
2022-08-22 03:58:55 -0400

    Added 2nd Program as a comment since it requires an answer

---
+++ src/model/Second_Program.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Second_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * IF this code didn't have any errors, then the output would be 2. 
+		 * 
+		 * The error comes from having a different variable name for the array on two lines of code
+		 * that intend to call the same array. exampleArray[], differs from example*S*Array[].
+		 * ----------- SO, the code would error out because of this --------------------
+		 * If that error got corrected, then it would error out for the FOR loop syntax. 
+		 * On the conditional for number of repetitions "i<exampleArray>" the last angle bracket 
+		 * is considered an error since it's not supposed to be there. so that would be another error.
+		 * 
+		 * If these two would get correted then the output would be 2 because the variable "i" iterates
+		 * twice until the index of the Array hits 2 (which belongs to the value 6). This will force the
+		 * conditional inside the IF statement to fail after the maximum value has been assigned to 6. 
+		 * 
+		 * Always good to keep in mind that the index of the element that corresponds to 6 on the array,
+		 * is 2, thus why the "index" gets that last value of 2 (which is what the program is printing.  
+		 *
+		 */
+
+	}
+
+}
